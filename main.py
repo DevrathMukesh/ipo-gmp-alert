059e217cc721666ed88bbbedeac9ddd8885efc86
DevrathMukesh <officialmukeshdevrath@gmail.com>
2026-07-19 00:34:19 +0530

    Sec commit

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("IPO Alert Bot Started")
+import os
+import requests
+
+TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
+CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+response = requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": "✅ IPO GMP Alert Bot is working!"
+    },
+)
+
+print(response.status_code)
+print(response.text)
